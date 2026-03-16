2f071d23b59bb6991e7d08c5606fcd2b7985b9c8
ASHOKA M <ashokashokm2001@gmail.com>
2026-03-16 20:07:17 +0530

    day 4 and day 5 class code

---
+++ Day-5 Class/script.js
@@ -0,0 +1,91 @@
+// num = 10;
+
+// num$ber = "java";
+// console.log(num);
+// console.log(num$ber);
+
+// class Demo {
+//   display() {
+//     console.log("This is demo class ");
+//   }
+// }
+
+// new Demo().display()
+
+// a=10;
+// b='10'
+
+// /*
+// console.log(a===b);
+// result =10>20?true:false
+// console.log(result);
+// */
+// //console.log(10+20+'java');
+
+
+
+// let a;
+// a=10;
+// let b=50;
+// b="java"
+
+// // let b=500
+// console.log(a);
+// console.log(b);
+
+// var num=50;
+// var str;
+// str="javascript"
+// str="node js"
+
+// console.log(num);
+// console.log(str);
+
+// var str=true;
+
+// console.log(str);
+
+
+// console.log(m);
+// var m=10;
+
+
+// const p=10;
+// p=20
+// console.log(p);
+
+
+
+
+
+//Data types: 
+
+
+let num1=10;
+// let str='j'
+let bool=true;
+let n=null;
+let s=Symbol(10)
+let n1=5142968354686n
+console.log(typeof num1);
+// console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof null);
+console.log(typeof s);
+console.log(typeof n1);
+
+
+
+let str="Javascript is the script  ${10+20}";
+let str1='Javascript  ${10+20}';
+let str2=`Hello
+            Every one: ${10+20}`
+console.log(str);
+console.log(str1);
+console.log(str2);
+
+
+
+
+
+
